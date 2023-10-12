a1b06204e15fa8543fa4a7acee7749f6faf1a4d2
Connor Vosberg <connorjv@gmail.com>
2023-10-11 19:03:41 -0700

    add validations for ClimbingSet model

--- app/models/climbing_set.rb
+++ app/models/climbing_set.rb
@@ -1,3 +1,8 @@
 class ClimbingSet < ApplicationRecord
     has_many :climbs
+
+    validates :set_name, presence: true
+    validates :week, presence: true
+    validates :date_start, presence: true
+    validates :date_end, presence: true
 end
