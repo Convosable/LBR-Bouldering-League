a3676013fe3f542f3f537bcbdf777bc8760d1d4c
Connor Vosberg <connorjv@gmail.com>
2023-10-11 19:00:30 -0700

    add validations for Climb model

--- app/models/climb.rb
+++ app/models/climb.rb
@@ -2,4 +2,7 @@ class Climb < ApplicationRecord
     has_many :user_climbs
     has_many :users, through: :user_climbs
     belongs_to :climbing_set
+
+    validates :grade, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
+    validates :color, presence: true
 end
