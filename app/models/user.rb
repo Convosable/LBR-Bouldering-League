9bdf9977565410386b74b7c928ddb25a9c41647b
Connor Vosberg <connorjv@gmail.com>
2023-10-11 18:55:51 -0700

    add validations for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ApplicationRecord
     belongs_to :team
     has_many :user_climbs
     has_many :climbs, through: :user_climbs
+
+    has_secure_password
+
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+    validates :handicap, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10  }
 end
