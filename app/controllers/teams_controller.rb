4846658fb6964b77af5435a6e7e4808895cc9c4d
Connor Vosberg <connorjv@gmail.com>
2023-10-12 19:12:50 -0700

    add full CRUD routes in TeamsController

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,4 +1,39 @@
 class TeamsController < ApplicationController
 
-    
+    def index
+        render json: Team.all.order(:team_name), status: :ok
+    end
+
+    def show
+        render json: find_team, status: :ok
+    end
+
+    def create
+        new_team = Team.create!(team_params)
+        render json: new_team, status: :created
+    end
+
+    def update
+        team = find_team
+        team.update!(team_params)
+        render json: team, status: :accepted
+    end
+
+    def destroy
+        team = find_team
+        team.delete
+        head :no_content
+    end
+
+
+    private
+
+    def find_team
+        Team.find(params[:id])
+    end
+
+    def team_params
+        params.permit(:team_name)
+    end
+
 end
