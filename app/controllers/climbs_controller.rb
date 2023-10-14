fa4fe8268cbe8d146109952fe0f28c5439e21b8d
Connor Vosberg <connorjv@gmail.com>
2023-10-13 18:55:06 -0700

    add full CRUD routes in ClimbsController

--- app/controllers/climbs_controller.rb
+++ app/controllers/climbs_controller.rb
@@ -1,2 +1,41 @@
 class ClimbsController < ApplicationController
+
+    def index
+        render json: Climb.all.order(:grade), status: :ok
+    end
+
+    def show
+        render json: find_climb, status: :ok
+    end
+
+    def create
+        new_climb = find_climbing_set.climbs.create!(climb_params)
+        render json: new_climb, status: :created
+    end
+
+    def update
+        climb = find_climbing_set.climbs.find(params[:id])
+        climb.update!(climb_params)
+        render json: climb, status: :accepted
+    end
+
+    def destroy
+        climb = find_climbing_set.climbs.find(params[:id])
+        climb.delete
+        head :no_content
+    end
+
+    private
+
+    def find_climb
+        Climb.find(params[:id])
+    end
+
+    def climb_params
+        params.permit(:grade, :color)
+    end
+
+    def find_climbing_set
+        ClimbingSet.find(params[:id])
+    end
 end
