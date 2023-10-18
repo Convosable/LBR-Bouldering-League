f302e46fe4ca2d2449f7f2debbc3e0dafcd2a31f
Connor Vosberg <connorjv@gmail.com>
2023-10-18 13:52:24 -0700

    update completedClimb state proberly upon filling out a form

--- client/src/pages/ClimbingSetDetails.js
+++ client/src/pages/ClimbingSetDetails.js
@@ -1,30 +1,54 @@
-import React from 'react'
+import React, { useState} from 'react'
 import { useParams } from 'react-router-dom';
 
 import { useSelector } from 'react-redux';
 
 const ClimbingSetDetails = () => {
 
+    const [completedClimbs, setCompletedClimbs] = useState([]);
     const {setName} = useParams()
 
-    const climbingSets = useSelector(state => state.climbingSets)
 
+    const climbingSets = useSelector(state => state.climbingSets)
     const set = climbingSets?.find(set => set.set_name === setName)
+
+    function handleClimbToggle(climbId) {
+        if (completedClimbs.includes(climbId)) {
+            setCompletedClimbs(completedClimbs.filter(id => id !== climbId))
+        } else {
+            setCompletedClimbs([...completedClimbs, climbId])
+        }
+    }
+
+    console.log(completedClimbs)
+
+    // on submit, i need to handle a adding the completion status of climb to the user
+
+
     if(!set) return <h1>Loading...</h1>
 
-    console.log(set)
-
-
-  return (
-    <div>
-        <h1>Week {set.week} - {set.set_name}</h1>
-        <p>Image here for set picture via active storage</p>
-        <h2>{set.date_start} - {set.date_end}</h2>
-        {set.climbs.map((climb) => (
-            <h1>{climb.color} V{climb.grade}</h1>
-        ))}
-    </div>
-  )
+    return (
+        <div>
+            <h1>Week {set.week} - {set.set_name}</h1>
+            <p>Image here for set picture via active storage</p>
+            <h2>{set.date_start} - {set.date_end}</h2>
+            <h3></h3>
+            <form>
+                {set.climbs.map((climb) => (
+                    <div key={climb.id}>
+                        <h1>{climb.color} V{climb.grade}</h1>
+                        <label>Completed: </label>
+                        <input 
+                            type="checkbox" 
+                            checked={completedClimbs.includes(climb.id)}
+                            onChange={() => handleClimbToggle(climb.id)}
+                        />
+                    </div>
+                ))}
+                <input type='submit'/>
+            </form>
+        </div>
+    )
 }
 
 export default ClimbingSetDetails;
