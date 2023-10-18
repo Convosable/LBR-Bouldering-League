677e68fecb92d780d68b77ed3b879c28aaf43d53
Connor Vosberg <connorjv@gmail.com>
2023-10-18 12:09:06 -0700

    update ClimbingSets page to display all climbing sets

--- client/src/pages/ClimbingSets.js
+++ client/src/pages/ClimbingSets.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import { useSelector } from 'react-redux'
 
@@ -11,8 +12,23 @@ const ClimbingSets = () => {
   return (
     <div>
         <h1> Climbing Sets</h1>
+        {climbingSets.map((set) => (
+            <div key={set.id}>
+                <h1>Week: {set.week} - {set.set_name}</h1>
+                <p>Include an image of the set via Active Storage here</p> 
+                <Link to={`/climbing_sets/${set.set_name}`}>
+                    <p>Set Details / Log climbs</p>
+                </Link>
+            </div>
+        ))}
     </div>
   )
 }
 
 export default ClimbingSets;
+
+//oirder by week (descendingf) 
+
+//display all climbing sets week adn inam,ge with link to log climbs for specific week
+
+//look into getting rid of %20 space in url (ex) http://localhost:4000/climbing_sets/Beer%20Wall)
