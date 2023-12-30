4add0bf744b331d05b518b137e8edc70193f455d
Connor Vosberg <connorjv@gmail.com>
2023-12-30 10:39:19 -0800

    adds a dropdown menu instead of a navbar for smaller screens

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -25,15 +25,40 @@ const NavBar = () => {
         })
     }
 
+    function handleNavigate(url) {
+        if (url === "/logout") {
+            handleLogout();
+        } else {
+            navigate(url)
+        }
+    }
+
     return (
-        <div className='navbar'>
-            <img src={logo} alt='lbr-logo-bubble' />
-            <NavLink to="/" exact="true">Home</NavLink>
-            <NavLink to="/leaderboards" exact="true">Leaderboards</NavLink>
-            <NavLink to="/climbing_sets" exact="true">Climbing Sets</NavLink>
-            <NavLink to="/teams/new" exact="true">Team Signup</NavLink>
-            {user.admin === true ? <NavLink to="/admin-tools" exact="true">Admin Tools</NavLink> : <NavLink to={`/${user.username}`}exact="true">Profile</NavLink>}
-            <button onClick = {() => handleLogout()}>Logout</button>
+        <div>
+            <div className='navbar'>
+                <img src={logo} alt='lbr-logo-bubble' />
+                <NavLink to="/" exact="true">Home</NavLink>
+                <NavLink to="/leaderboards" exact="true">Leaderboards</NavLink>
+                <NavLink to="/climbing_sets" exact="true">Climbing Sets</NavLink>
+                <NavLink to="/teams/new" exact="true">Team Signup</NavLink>
+                {user.admin === true ? 
+                <NavLink to="/admin-tools" exact="true">Admin Tools</NavLink> : 
+                <NavLink to={`/${user.username}`}exact="true">Profile</NavLink>}
+                <button onClick = {() => handleLogout()}>Logout</button>
+            </div>
+            <div className='dropdown'>
+                <img src={logo} alt='lbr-logo-bubble' />
+                <select onChange={(e) => handleNavigate(e.target.value)}>
+                    <option value = "/">Home</option>
+                    <option value = "/leaderboards">Leaderboards</option>
+                    <option value = "/climbing_sets">Climbing Sets</option>
+                    <option value = "/teams/new">Team Signup</option>
+                    <option value = {user.admin === true ? "/admin-tools" : `/${user.username}`}>
+                        {user.admin === true ? "Admin Tools" : "Profile"}
+                    </option>
+                    <option value = "/logout">Logout</option>
+                </select>
+            </div>
         </div>
     )
 }
